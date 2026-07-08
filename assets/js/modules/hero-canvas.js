ea8b2b89224b1b9c093d947fe911ce2e4076a7e0
Celso Trabalho <celsortc@outlook.com>
2026-07-08 11:04:25 -0300

    style: refactor some code, use calc to height..

---
+++ assets/js/modules/hero-canvas.js
@@ -0,0 +1,16 @@
+// export default function initHeroCanvas() {
+//   const canvas = document.getElementById("hero-canvas");
+//   const ctx = canvas.getContext("2d");
+
+//   function resizeCanvas() {
+//     canvas.width = canvas.offsetWidth;
+//     canvas.height = canvas.offsetHeight;
+//   }
+//   resizeCanvas();
+//   window.addEventListener("resize", resizeCanvas);
+
+//   ctx.beginPath();
+//   ctx.arc(100, 100, 5, 0, Math.PI * 2);
+//   ctx.fillStyle = "#FFFFFF";
+//   ctx.fill();
+// }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
 import initNavbarScroll from "./modules/navbarScroll.js";
+import initHeroCanvas from "./modules/hero-canvas.js";
 
 initNavbarScroll();
+initHeroCanvas();
