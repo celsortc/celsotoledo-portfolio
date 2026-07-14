7d8ee7cf71d8f050f069af089c5e2a2244cdc518
Celso Trabalho <celsortc@outlook.com>
2026-07-14 13:01:23 -0300

    trying to import motion and remove log from navbar js

--- assets/js/modules/navbarScroll.js
+++ assets/js/modules/navbarScroll.js
@@ -1,8 +1,6 @@
 export default function initNavbarScroll() {
   const header = document.querySelector(".nav-desktop");
 
-  console.log(header);
-
   window.addEventListener("scroll", () => {
     if (window.scrollY > 25) {
       header.classList.add("scrolled");
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+import {
+  animate,
+  scroll,
+} from "https://cdn.jsdelivr.net/npm/motion@latest/+esm";
+
 import initNavbarScroll from "./modules/navbarScroll.js";
 // import initHeroCanvas from "./modules/hero-canvas.js";
 
