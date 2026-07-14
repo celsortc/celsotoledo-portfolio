afda52f7d8d8a7e8d5ef08dbaf271de6a9932407
Celso Trabalho <celsortc@outlook.com>
2026-07-14 15:34:53 -0300

    efeito do header e hero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 import {
   animate,
   scroll,
+  inView,
+  hover,
+  press,
 } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm";
 
 import initNavbarScroll from "./modules/navbarScroll.js";
@@ -8,3 +11,40 @@ import initNavbarScroll from "./modules/navbarScroll.js";
 
 initNavbarScroll();
 // initHeroCanvas();
+
+// animate("h1", { opacity: [0, 1] }, { duration: 2 });
+// animate("h1", { rotate: 360 }, { duration: 2 });
+// animate(".about-icon-desc", { y: 0, opacity: 1 }, { delay: stagger(0.1) });
+
+// animate(
+//   ".nav-pill",
+//   {
+//     opacity: [0, 1],
+//     y: [-20, 0],
+//   },
+//   {
+//     duration: 0.6,
+//   },
+// );
+
+// animação nav e hero
+
+async function playHeroThenNav() {
+  const heroText = animate(
+    ".hero-text",
+    { opacity: 1, x: 0 },
+    { duration: 0.6 },
+  );
+  const heroPhoto = animate(
+    ".img-container",
+    { opacity: 1, x: 0 },
+    { duration: 0.6 },
+  );
+
+  // espera as duas animações terminarem
+  await Promise.all([heroText.finished, heroPhoto.finished]);
+
+  animate(".nav-pill", { opacity: 1, y: 0 }, { duration: 0 });
+}
+
+playHeroThenNav();
