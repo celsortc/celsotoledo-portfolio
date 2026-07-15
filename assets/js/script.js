a7e38d34385536a9ea34f87662733671bbc5cbd3
Celso Trabalho <celsortc@outlook.com>
2026-07-15 11:37:06 -0300

    indicating hero.js location

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,47 +7,12 @@ import {
 } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm";
 
 import initNavbarScroll from "./modules/navbarScroll.js";
+import initHero from "./modules/hero.js";
 // import initHeroCanvas from "./modules/hero-canvas.js";
 
 initNavbarScroll();
+initHero();
 // initHeroCanvas();
-
-// animate("h1", { opacity: [0, 1] }, { duration: 2 });
-// animate("h1", { rotate: 360 }, { duration: 2 });
-// animate(".about-icon-desc", { y: 0, opacity: 1 }, { delay: stagger(0.1) });
-
-// animate(
-//   ".nav-pill",
-//   {
-//     opacity: [0, 1],
-//     y: [-20, 0],
-//   },
-//   {
-//     duration: 0.6,
-//   },
-// );
-
-// animação nav e hero
-
-async function playHeroThenNav() {
-  const heroText = animate(
-    ".hero-text",
-    { opacity: 1, x: 0 },
-    { duration: 0.6 },
-  );
-  const heroPhoto = animate(
-    ".img-container",
-    { opacity: 1, x: 0 },
-    { duration: 0.6 },
-  );
-
-  // espera as duas animações terminarem
-  await Promise.all([heroText.finished, heroPhoto.finished]);
-
-  animate(".nav-pill", { opacity: 1, y: 0 }, { duration: 0 });
-}
-
-playHeroThenNav();
 
 inView(".title-block h2", (element) => {
   animate(element, { opacity: 1, y: 0 }, { duration: 0.6 });
