ac87a51d11e2e9b0953ed832f4b9d610e7ad59cd
Celso Trabalho <celsortc@outlook.com>
2026-07-16 12:25:56 -0300

    feat: complete translate button, now he has initial language, and verify language with localStorage

--- assets/js/modules/translate.js
+++ assets/js/modules/translate.js
@@ -14,11 +14,55 @@ export default function initTranslate() {
     },
   };
 
+  function setInitialLanguage() {
+    //define lingua padrão como pt, caso não tenha nada no localStorage, se tiver, pega a lingua salva no localStorage
+    const nativeLang = "pt";
+    const firstLang =
+      localStorage.getItem("lang") != null
+        ? localStorage.getItem("lang")
+        : nativeLang;
+
+    //verifica se a primeira lingua é pt ou en, se for pt, o botão de troca de lingua deve mostrar EN, se for en, deve mostrar PT, altera dataset também
+    if (firstLang === "en") {
+      document.querySelector(".btn-language").textContent = "PT";
+      document.querySelector(".btn-language").dataset.lang = "en";
+    }
+
+    //define a primeira lingua do site com base no ternário lá em cima
+    document.querySelectorAll("[data-i18n]").forEach((element) => {
+      const key = element.getAttribute("data-i18n");
+
+      if (translations[firstLang][key]) {
+        element.textContent = translations[firstLang][key];
+      }
+    });
+  }
+
+  setInitialLanguage();
+
+  function verifySavedLanguage(currentLanguage) {
+    if (localStorage.getItem("lang") !== null) {
+      return localStorage.getItem("lang") === "pt" ? "en" : "pt";
+    } else {
+      return currentLanguage === "pt" ? "en" : "pt";
+    }
+  }
+
   const translateBtn = document.querySelector(".btn-language");
 
   translateBtn.addEventListener("click", () => {
-    const currentLang = translateBtn.dataset.lang;
-    const newLang = currentLang === "en" ? "pt" : "en";
+    const currentLang = translateBtn.dataset.lang; //pt
+
+    const newLang = verifySavedLanguage(currentLang);
+
+    // if (
+    //   localStorage.getItem("lang") === "pt" ||
+    //   localStorage.getItem("lang") === "en"
+    // ) {
+    //   newLang = localStorage.getItem("lang") === "en" ? "pt" : "en";
+    // } else {
+    //   newLang = ;
+    // }
 
     document.querySelectorAll("[data-i18n]").forEach((element) => {
       const key = element.getAttribute("data-i18n");
@@ -27,7 +71,9 @@ export default function initTranslate() {
         element.textContent = translations[newLang][key];
       }
     });
-    translateBtn.textContent = newLang === "en" ? "PT" : "EN";
+    translateBtn.textContent = newLang === "pt" ? "EN" : "PT";
     translateBtn.dataset.lang = newLang;
+
+    localStorage.setItem("lang", newLang);
   });
 }
