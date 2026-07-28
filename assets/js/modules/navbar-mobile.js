ce1410043836682f06b875213dcd3e4c237d1faa
Celso <celsortc@outlook.com>
2026-07-27 22:17:08 -0300

    feat: starting js creation

--- assets/js/modules/navbar-mobile.js
+++ assets/js/modules/navbar-mobile.js
@@ -1,1 +1,14 @@
-export default function initNavbarMobile() {}
+export default function initNavbarMobile() {
+  const menuHamburger = document.getElementById("hamburgerbtn");
+  const navOverlay = document.querySelector(".nav-overlay");
+
+  function toggleMenu() {
+    const isOpen = menuHamburger.classList.toggle("is-open");
+
+    console.log(isOpen);
+
+    navOverlay.classList.toggle("is-open", isOpen);
+  }
+
+  menuHamburger.addEventListener("click", toggleMenu);
+}
